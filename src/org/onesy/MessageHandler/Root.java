51cc496dd9c1136a37d317135fdc159bf7df83d2
onesy <leentingOne@gmail.com>
2013-01-07 20:27:11 +0800

    on no

---
+++ src/org/onesy/MessageHandler/Root.java
@@ -0,0 +1,34 @@
+package org.onesy.MessageHandler;
+
+public class Root extends MessageHandlerBase {
+
+	public Root(String Message) {
+		super(Message);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Process(String content) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ProcessContent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pull() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
